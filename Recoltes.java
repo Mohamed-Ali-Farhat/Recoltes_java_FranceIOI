769af810fed1fccc55816c2c5a26df4d52f3774d
Mohamed Ali Farhat <100702507+MohamedAliFathat@users.noreply.github.com>
2024-02-02 18:28:44 +0000

    solution

---
+++ Recoltes.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    static Scanner entrée = new Scanner(System.in);
+    public static void main(String[] args) {
+        int nb= entrée.nextInt();
+
+
+        System.out.println(nb*nb*23);
+    }
+}
